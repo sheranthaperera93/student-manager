f38075b107e8d7b0c50495ca689cfc5bc8526b92
Sherantha Perera <sherantha.perera@gmail.com>
2025-02-20 17:26:26 +0530

    feat: add Docker support with Dockerfile and .dockerignore, configure TypeORM with environment variables, and update docker-compose for service orchestration

--- CourseService/src/app.module.ts
+++ CourseService/src/app.module.ts
@@ -1,22 +1,26 @@
 import { Module } from '@nestjs/common';
 import { CourseModule } from './course/course.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'sheranthap',
-      password: 'Test@123',
-      database: 'studentmanagement',
-      synchronize: false, // Enable auto-sync
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      migrations: [__dirname + '/migrations/*{.ts,.js}'],
-      migrationsRun: true,
-      autoLoadEntities: true,
-      logging: true,
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USERNAME'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DATABASE'),
+        synchronize: false, // Enable auto-sync
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/*{.ts,.js}'],
+        migrationsRun: true,
+        autoLoadEntities: true,
+        logging: true,
+      }),
+      inject: [ConfigService]
     }),
     CourseModule,
   ],
