c73c00794df3315b5a40ea9c4e1c001e967b6def
sheranthapftd <sheranthap@fortude.co>
2025-03-06 12:01:35 +0530

    Removed unwanted import

--- CourseService/src/course/course.resolver.ts
+++ CourseService/src/course/course.resolver.ts
@@ -1,12 +1,4 @@
-import {
-  Args,
-  ID,
-  Int,
-  Mutation,
-  Query,
-  Resolver,
-  ResolveReference,
-} from '@nestjs/graphql';
+import { Args, ID, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Course } from '../entities/course.entity';
 import { CourseService } from './course.service';
 import { PaginatedCourses } from './models/paginated-courses.model';
@@ -55,5 +47,4 @@ export class CourseResolver {
     };
     return response;
   }
-
 }
